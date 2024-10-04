705ab859b66132b76a108c7d458bfc9e52e9853b
= <=>
2024-10-04 09:27:23 -0300

    adding composition api to remove ref error

--- src/components/ComponentPagination.vue
+++ src/components/ComponentPagination.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref } from '@vue/composition-api';
 
 export default defineComponent({
   props: {
