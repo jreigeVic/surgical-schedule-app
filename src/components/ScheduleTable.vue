a9dfda92264afcdb96599195eef7de1695d45193
= <=>
2024-10-04 07:53:04 -0300

    feat: adding first steps to sort schedule table on asc or desc

--- src/components/ScheduleTable.vue
+++ src/components/ScheduleTable.vue
@@ -2,21 +2,51 @@
   <table class="min-w-full table-auto border-collapse border bg-[#FAFAFA] border-gray-300">
     <thead>
       <tr class="bg-finx-gray">
-        <th class="text-left p-2 sm:p-4 " @click="sort('medico')">Médico</th>
-        <th class="text-left p-2 sm:p-4 " @click="sort('paciente')">Paciente</th>
-        <th class="text-left p-2 sm:p-4 " @click="sort('dataCriacao')">Data de Criação</th>
+        <th class="text-left p-2 sm:p-4 cursor-pointer" @click="sort('medico')">
+          Médico
+          <span v-if="sortKey === 'medico'">
+            <svg v-if="sortOrder === 'asc'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
+            </svg>
+            <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
+          </span>
+        </th>
+        <th class="text-left p-2 sm:p-4 cursor-pointer" @click="sort('paciente')">
+          Paciente
+          <span v-if="sortKey === 'paciente'">
+            <svg v-if="sortOrder === 'asc'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
+            </svg>
+            <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
+          </span>
+        </th>
+        <th class="text-left p-2 sm:p-4 cursor-pointer" @click="sort('dataCriacao')">
+          Data de Criação
+          <span v-if="sortKey === 'dataCriacao'">
+            <svg v-if="sortOrder === 'asc'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
+            </svg>
+            <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
+          </span>
+        </th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="schedule in sortedSchedules" :key="schedule.id">
         <td class="p-2 sm:p-4 border border-gray-300">{{ schedule.medico.nome }}</td>
-        <td class="p-2 sm:p-4 border border-gray-300">{{ schedule.paciente.nome }} - {{ new
-          Date(schedule.paciente.dataNascimento).toLocaleDateString() }}</td>
+        <td class="p-2 sm:p-4 border border-gray-300">{{ schedule.paciente.nome }} - {{ new Date(schedule.paciente.dataNascimento).toLocaleDateString() }}</td>
         <td class="p-2 sm:p-4 border border-gray-300">{{ new Date(schedule.dataCriacao).toLocaleDateString() }}</td>
       </tr>
     </tbody>
   </table>
 </template>
+
 
 <script>
 export default {
